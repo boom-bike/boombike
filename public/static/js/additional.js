453601b08495a0a2d5b8ea86794c83e7a7998679
quentin <udevel@udevel-VirtualBox.(none)>
2014-02-21 10:46:01 +0100

    content update 3

---
+++ public/static/js/additional.js
@@ -0,0 +1,19 @@
+// Home
+$(function() {
+//    if (window.location.pathname == "/") {
+//        $("#header").children("h1").after("<h2><a href=\"home_details\">Par ici >>></a></h2>");
+
+//        $(".article_content").children(".cyclotourism").click(function(event) {
+//            window.location.href = "home_details#cyclotourism";
+//        });
+//
+//        $(".article_content").children(".boom").click(function(event) {
+//            window.location.href = "home_details#boom";
+//        });
+//
+//        $(".article_content").children(".boom_and_bike").click(function(event) {
+//            window.location.href = "home_details#boom_and_bike";
+//        });
+//    }
+});
+
